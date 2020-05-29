7b714b71e985e55fb9093d28ece067820d63d218
jose <josealves_w@yahoo.com.br>
2020-05-29 14:48:22 -0300

    salvando categorias

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const admin = require("./rotes/admin")
 const path = require("path")
 const app = express()
-//const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
 //configuraçoes:
 //body parser
@@ -14,6 +14,17 @@ app.use(bodyParser.json())
 //config TemplateEngine
     app.engine("handlebars", handlebars({defaultLayout:"main"}))
     app.set("view engine", "handlebars")
+
+//mongo   
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/estudos",{
+    useNewUrlParser:true, useUnifiedTopology: true
+}).then(
+    ()=>{console.log("connectado mongoDB com sucesso")}
+).catch((err) =>{
+    console.log("Houve um erro"+err)
+})
 
 //public
 //app.use(express.static(path.join(__dirname,"public")))   
--- rotes/admin.js
+++ rotes/admin.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require("mongoose")
+require("../models/Categoria")
+const Categoria = mongoose.model("categorias")
+
 
 
 router.get('/',(req,res) =>{
     res.render("admin/index")
+})
+
+router.post('/categorias/nova',(req,res) =>{
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(()=>{
+        console.log("Categoria salva com sucesso")
+        res.send("categoria salvo com sucesso")
+    }).catch((err)=>{
+        console.log("Erro ao salvar categoria"+err)
+        res.send("Erro ao salvar categoria:"+req)
+    })
 })
 
 router.get('/posts',(req,res) =>{
